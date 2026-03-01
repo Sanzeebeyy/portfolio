0aab610ae2095dd84535b78de954c2a372334df0
Sanjib <sanjibpoudel49@gmail.com>
2026-03-01 20:37:03 +0545

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className="footer-content">
                 <div className="footer-logo">SP.</div>
                 <p className="footer-text">
-                    Designed and Built with <FaHeart className="heart-icon" /> by Sanjib Poudel
+                    Designed and Developed with <FaHeart className="heart-icon" /> by Sanjib Poudel
                 </p>
                 <div className="footer-socials">
                     <a href="#"><FaGithub /></a>
